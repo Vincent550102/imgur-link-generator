a91fc43401e9a2e0b2ff74f8bbd2025bf1e38dbf
Vincent550102 <5020559@gmail.com>
2023-03-07 20:59:59 +0000

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,79 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
-
+  <v-card title="圖片網址產生器" class="mx-auto mt-4" subtitle="將您的圖片變為 imgur 的網址" text="您可以選擇用下方區塊上傳或將圖片拖放或貼上到此區塊！"
+    max-width="500" variant="outlined" :loading="loading">
+    <v-card-text>
+      <v-file-input :disabled="disabled" accept="image/png, image/jpeg, image/bmp" placeholder="Upload your images!"
+        prepend-icon="mdi-image" label="Image" density="compact" @change="showFile"></v-file-input>
+    </v-card-text>
+    <v-card-actions class="float-right">
+      <v-btn @click="submit"><v-icon icon="mdi-check"></v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+  <meowCard :url="uploadFinish.url" v-for="uploadFinish in uploadFinishs" v-bind:key="uploadFinish.title">
+  </meowCard>
+  <!-- <meowCard /> -->
+</template> 
 <script>
+import meowCard from './Card.vue'
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  components: {
+    meowCard
+  },
+  data() {
+    return {
+      file: null,
+      fs: {
+        name: '',
+        thumbnail: null,
+        size: null
+      },
+      title: '',
+      des: '',
+      loading: false,
+      disabled: false,
+      uploadFinishs: []
+    }
+  },
+  methods: {
+    showFile(e) {
+      this.file = e.target.files[0];
+      this.fs.name = this.file.name;
+      console.log(this.fs.name)
+      this.fs.size = Math.floor(this.file.size * 0.001) + 'KB';
+      this.fs.thumbnail = window.URL.createObjectURL(this.file);
+      this.title = this.fs.name;
+    },
+    submit() {
+      console.log(this.title);
+      const token = "35ac59e22bb084643ec26d0d179365f7ad9e2c5d";
+
+      let form = new FormData();
+      form.append('image', this.file);
+      //form.append('title', this.title);
+      //form.append('description', this.des);
+
+      this.loading = true;
+      this.disabled = true;
+      fetch("https://api.imgur.com/3/image", {
+        body: form,
+        headers: {
+          'Authorization': 'Bearer ' + token
+        },
+        method: 'POST'
+      })
+        .then(resp => resp.json())
+        .then(res => {
+          console.log(res.data.link)
+          this.uploadFinishs.unshift({
+            title: this.title,
+            url: res.data.link
+          })
+          this.loading = false
+          this.disabled = false
+        });
+    }
+  },
+
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
